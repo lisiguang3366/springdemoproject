afb29e56c4705e02c78f42013153012755eb48d7
lisiguang3366 <69197171+lisiguang3366@users.noreply.github.com>
2020-08-05 19:42:50 +0800

    Update SpringDemoApplication.java

--- spring-demo/src/main/java/com/kgc/SpringDemoApplication.java
+++ spring-demo/src/main/java/com/kgc/SpringDemoApplication.java
@@ -11,6 +11,7 @@ public class SpringDemoApplication {
     public static void main(String[] args) {
          //启动操作
         //啦啦啦啦
+        //组员b的修改
         SpringApplication.run(SpringDemoApplication.class, args);
     }
 
